87cdfa42966fdc1cd96d742f82e1134efdcd0eb7
otavioag <otavio.luis.aguiar@gmail.com>
2017-06-03 18:47:58 -0300

    process data done

--- central-sensor/src/main.c
+++ central-sensor/src/main.c
@@ -1,9 +1,9 @@
-/* Master sensor source file 
-	
+/* Master sensor source file
+
 	Felipe Alegria Rollo Dias	- 9293501
 	Giovanna Oliveira Guimarães	- 9293693
 	Lucas Alexandre Soares		- 9293265
-	Otavio Luiz Aguiar			- 9293518
+	Otavio Luis de Aguiar		- 9293518
 */
 
 // Std's
@@ -32,6 +32,15 @@
 #define LOOPBACK_ADDR 	INADDR_LOOPBACK
 #define BROADCAST_ADDR 	INADDR_BROADCAST
 
+using namespace std;
+
+int fuel;
+int humidity;
+double pressure;
+double temperature;
+int gpsa;
+double gpsla;
+double gpslo;
 
 // void die(const char *s, char *data, );
 void die(const char *s);
@@ -43,11 +52,11 @@ void ProcessData(char *data, int length);
 	char *data;						// Data buffer
 int main(int argc, char **argv){
 
-    
+
     struct sockaddr_in socket_addr;	// Server
     struct sockaddr_in client_addr; // Clients
     socklen_t client_size = sizeof(client_addr);
-    
+
     int data_length;
 
 	data = (char *) malloc(sizeof(char)*BUFFSIZE);
@@ -57,57 +66,94 @@ int main(int argc, char **argv){
 
 	// Initialize server socket
     memset((char *) &socket_addr, 0, sizeof(socket_addr));
-     
+
     socket_addr.sin_family = IPV4;
     socket_addr.sin_port = htons(PORT);
     socket_addr.sin_addr.s_addr = htonl(ANY_ADDR); // Receive from any address
-     
+
     // Bind server socket to port
     if(bind(udp_socket, (struct sockaddr *) &socket_addr, sizeof(socket_addr)) < 0)
         die("Failed to bind server socket");
-    
-    // Create a user interface in seperate 
+
+    // Create a user interface in seperate
     // thread_descriptor = pthread_create(thread, NULL, &ListenLocalCommand, NULL);
 
     // Keep listening for data forever
     while(true){
-     
+
         fflush(stdout); // Force flush from stdout internal buffering
-        
+
         // Clear data data before getting more
         memset(data, 0, BUFFSIZE);
         // Get data - blocking call as scanf or getchar etc
-        if( (data_length = recvfrom(udp_socket, data, BUFFSIZE, 
-        							0, (struct sockaddr *) 
+        if( (data_length = recvfrom(udp_socket, data, BUFFSIZE,
+        							0, (struct sockaddr *)
         							&client_addr, &client_size) ) < 0)
             fprintf(stderr, "Failed to get data");
-         
+
         // Print sender IP:PORT and data
-        printf("Received packet from %s:%d\n", 
+        printf("Received packet from %s:%d\n",
         	inet_ntoa(client_addr.sin_addr),
         	ntohs(client_addr.sin_port));
 
         printf("Data: %s\n" , data);
         ProcessData(data, data_length);
-         
+
     }
- 
+
     close(udp_socket);
 	return 0;
 }
 
 // Display error and exit
 void die(const char *s){
-	
-	if(udp_socket != -1) 
+
+	if(udp_socket != -1)
 		close(udp_socket);
-    
+
     perror(s);
     free(data);
     exit(errno);
 }
 
-void ProcessData(char *data, int length){
+void ProcessData(char *data, int length) {
+	char c;
+	int pos = 0;
+
+	while(data[pos] != '\\') pos++;
+
+	string sdata(data, pos);	//string that contains the data
+
+	++pos;
+
+	//detects the sensor the data came from
+	switch(data[pos]) {
+		case 'F':
+			fuel = stoi(sdata, NULL, 10);
+			break;
+		case 'G':
+			pos += 3;
+			if (data[pos] == 'A')
+				gpsa = stoi(sdata, NULL, 10);
+			else {
+				++pos;
+				if (data[pos] == 'A')
+					gpsla = stod(sdata, NULL);
+				else
+					gpslo = stod(sdata, NULL)
+			}
+			break;
+		case 'H':
+			humidity = stoi(sdata, NULL, 10);
+			break;
+		case 'P':
+			pressure = stod(sdata, NULL);
+			break;
+		case 'T':
+			temperature = stod(sdata, NULL);
+			break;
+	}
+
 
 }
 
